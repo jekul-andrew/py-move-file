1f6fc42dc59b28f8fe16b7d91a94182a9e639b44
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-07-05 12:40:20 +0300

    Py-move-file refactored

--- app/main.py
+++ app/main.py
@@ -1,50 +1,62 @@
 import os
 
 
+class CommandStringError(Exception):
+
+    def __str__(self) -> str:
+        return ("The command format must be: "
+                "mv [source file path] [destination file path]. "
+                "Use a single space to separate the arguments.")
+
+
+class WrongDestinationNameError(Exception):
+
+    def __str__(self) -> str:
+        return "Destination filename is wrong"
+
+
 def move_file(command: str) -> None:
 
     str_parts = command.strip().split(" ")
 
-    if len(str_parts) == 3 and str_parts[0] == "mv":
+    if len(str_parts) != 3 or str_parts[0] != "mv":
+        raise CommandStringError
 
-        cmd, origin, destination = str_parts
+    cmd, origin, destination = str_parts
 
-        if destination[-1] != "/":
-            destination = destination.strip("/ ")
-            path_list = destination.split("/")
+    # find separator at the end
+    if destination[-1] == os.sep:
+        raise WrongDestinationNameError
 
-            dest_filename = path_list.pop()
+    dir_path = os.path.dirname(destination)
 
-            created_dir = []
+    dest_filename = os.path.basename(destination)
 
-            for dirname in path_list:
-                if not created_dir:
-                    dir_path = dirname
-                else:
-                    dir_path = "/".join(created_dir) + "/" + dirname
+    created_dir = []
 
-                try:
-                    os.mkdir(dir_path)
+    if dir_path:
+        path_list = dir_path.split(os.sep)
+        for dirname in path_list:
 
-                except FileExistsError:
-                    pass
-
-                created_dir.append(dirname)
-
-            dir_path = "/".join(created_dir)
-
-            if not dir_path:
-                destination_path = dest_filename
+            if not created_dir:
+                dir_path = dirname
             else:
-                destination_path = dir_path + "/" + dest_filename
+                dir_path = os.path.join(*created_dir) + os.sep + dirname
 
-            try:
-                with (open(origin, "r") as source,
-                      open(destination_path, "w") as destination_file):
+            if not os.path.exists(dir_path):
+                os.mkdir(dir_path)
 
-                    destination_file.write(source.read())
+            created_dir.append(dirname)
 
-            except FileNotFoundError:
-                pass
-            else:
-                os.remove(origin)
+        dir_path = os.path.join(*created_dir)
+        destination_path = os.path.join(dir_path, dest_filename)
+
+    else:
+        destination_path = dest_filename
+
+    with (open(origin, "r") as source,
+          open(destination_path, "w") as destination_file):
+
+        destination_file.write(source.read())
+
+    os.remove(origin)
