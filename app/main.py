de0b99a4e6d4dd319164fcc2f36f2b4d7baf8896
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-07-05 13:20:37 +0300

    Py-move-file refactored 2

--- app/main.py
+++ app/main.py
@@ -9,10 +9,13 @@ class CommandStringError(Exception):
                 "Use a single space to separate the arguments.")
 
 
-class WrongDestinationNameError(Exception):
+class OriginFileNotExistsError(Exception):
+
+    def __init__(self, file_path: str = "") -> None:
+        self.file_path = file_path
 
     def __str__(self) -> str:
-        return "Destination filename is wrong"
+        return f"Origin file {self.file_path} doesn't exist on server"
 
 
 def move_file(command: str) -> None:
@@ -24,35 +27,21 @@ def move_file(command: str) -> None:
 
     cmd, origin, destination = str_parts
 
-    # find separator at the end
-    if destination[-1] == os.sep:
-        raise WrongDestinationNameError
+    if not os.path.exists(origin):
+        raise OriginFileNotExistsError(file_path=origin)
 
     dir_path = os.path.dirname(destination)
 
     dest_filename = os.path.basename(destination)
 
-    created_dir = []
+    # find separator at the end
+    if not dest_filename:
+        dest_filename = os.path.basename(origin)
 
     if dir_path:
-        path_list = dir_path.split(os.sep)
-        for dirname in path_list:
+        os.makedirs(dir_path, exist_ok=True)
 
-            if not created_dir:
-                dir_path = dirname
-            else:
-                dir_path = os.path.join(*created_dir) + os.sep + dirname
-
-            if not os.path.exists(dir_path):
-                os.mkdir(dir_path)
-
-            created_dir.append(dirname)
-
-        dir_path = os.path.join(*created_dir)
-        destination_path = os.path.join(dir_path, dest_filename)
-
-    else:
-        destination_path = dest_filename
+    destination_path = os.path.join(dir_path, dest_filename)
 
     with (open(origin, "r") as source,
           open(destination_path, "w") as destination_file):
