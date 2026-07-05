c2e7d28d7932c4c6a68e6c0f68cdd09d3bd5f3be
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-07-05 17:36:39 +0300

    Py-move-file refactored 5

--- app/main.py
+++ app/main.py
@@ -20,32 +20,27 @@ class OriginFileNotExistsError(Exception):
 
 def move_file(command: str) -> None:
 
-    str_parts = command.strip().split(" ")
+    str_parts = command.strip().split()
 
     if len(str_parts) != 3 or str_parts[0] != "mv":
         raise CommandStringError
 
-    cmd, origin, destination = str_parts
+    cmd, source, destination = str_parts
 
-    if not os.path.exists(origin) or not os.path.isfile(origin):
-        raise OriginFileNotExistsError(file_path=origin)
+    if not os.path.exists(source) or not os.path.isfile(source):
+        raise OriginFileNotExistsError(file_path=source)
 
     dir_path = os.path.dirname(destination)
 
     dest_filename = os.path.basename(destination)
 
     # find separator at the end
-    if not dest_filename:
-        dest_filename = os.path.basename(origin)
+    if destination[-1] == os.sep:
+        dest_filename = os.path.basename(source)
 
     if dir_path:
         os.makedirs(dir_path, exist_ok=True)
 
     destination_path = os.path.join(dir_path, dest_filename)
 
-    with (open(origin, "r") as source,
-          open(destination_path, "w") as destination_file):
-
-        destination_file.write(source.read())
-
-    os.remove(origin)
+    os.rename(source, destination_path)
